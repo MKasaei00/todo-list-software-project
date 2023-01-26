8f11b48d2924d043cc8f3a9316d943c0b29b4c50
Mohammad Kasaei <mohammadkasaei00@yahoo.com>
2023-01-27 00:19:19 +0330

    Add migration

---
+++ backend/ToDoListWebApi/Migrations/20230126204414_AddToDoListsTable.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ToDoListWebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddToDoListsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ToDoListObjects",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Content = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ToDoListObjects", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ToDoListObjects");
+        }
+    }
+}
